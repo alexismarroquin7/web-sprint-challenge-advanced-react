bfdf978af958165066748e03ba0677b099ffb167
Alexis Marroquin <alexismarroquin754@gmail.com>
2021-03-12 06:44:26 -0800

    CheckoutForm - refactored stateful logic

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-// import axios from "axios";
+
 import {getPlants} from "../services/plantsService"
 
 export default class PlantList extends Component {
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,21 @@
 // write your custom hook here to control your checkout form
+import {useState} from "react";
+
+const useForm = initialValues => {
+    const [values, setValues] = useState(initialValues);
+
+    const handleChange = e => {
+        setValues({
+            ...values,
+            [e.target.name]:e.target.values
+        });
+    };
+
+    const clearForm = () => {
+        setValues(initialValues);
+    };
+
+    return [values, handleChange, clearForm];
+}
+
+export default useForm;
