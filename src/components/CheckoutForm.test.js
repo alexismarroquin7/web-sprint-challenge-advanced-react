2735894ee2bf3b6c39bd5d99cbb828894cfee7e8
Alexis Marroquin <alexismarroquin754@gmail.com>
2021-03-12 07:29:11 -0800

    CheckoutForm test - form shows success message on submit with form details

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -16,5 +17,29 @@ test("form header renders", () => {
 });
 
 test("form shows success message on submit with form details", () => {
-    
+    //Arrange
+    render(<CheckoutForm />);
+
+    //Act
+    const fnameInput = screen.getByLabelText(/first name:/i);
+    const lnameInput = screen.getByLabelText(/last name:/i);
+    const addressInput = screen.getByLabelText(/address:/i);
+    const cityInput = screen.getByLabelText(/city:/i);
+    const stateInput = screen.getByLabelText(/state:/i);
+    const zipInput = screen.getByLabelText(/zip:/i);
+    const checkoutButton = screen.getByTestId("checkoutButton");
+
+    userEvent.type(fnameInput, "Alexis");
+    userEvent.type(lnameInput, "Marroquin");
+    userEvent.type(addressInput, "608 Walnut St");
+    userEvent.type(cityInput, "Roselle Park");
+    userEvent.type(stateInput, "NJ");
+    userEvent.type(zipInput, "01204");
+    userEvent.click(checkoutButton);
+
+    const successMessage = screen.getByTestId("successMessage");
+
+    //Assert
+    expect(successMessage).toBeInTheDocument();
+
 });
